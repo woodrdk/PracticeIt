0a7253f64e05325f693b342fd50e8c419b3c12b3
Robert Wood Jr <rdrwood@gmail.com>
2019-05-10 18:02:11 -0700

    setting up JS loops

---
+++ Javascript/Loops/loopMystery1.js
@@ -0,0 +1,15 @@
+
+// loopMystery1
+//Complete the following for loop to produce the following output:
+
+//-4
+//14
+//32
+//50
+//68
+//86
+
+for (var i = 1; i <= 6; i++) {
+    console.log(-22 + (i * 18));
+
+}
