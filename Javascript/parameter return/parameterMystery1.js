f1f5fff2194a65a3f3093da3a0840f2fd5ab61f3
Robert Wood Jr <rdrwood@gmail.com>
2019-05-11 20:24:16 -0700

    param return setup

---
+++ Javascript/parameter return/parameterMystery1.js
@@ -0,0 +1,27 @@
+﻿// parameterMystery1
+
+
+//Assume the following functions are defined:
+
+function main() {
+    var a = 1;
+    var b = 2;
+    var c = -3;
+    mystery(a, b, c);
+    mystery(c, 4, a);
+    mystery(a + b, b + c, c + a);
+}
+​
+function mystery(c, a, b) {
+    console.log(b + " + " + c + " = " + a);
+}
+
+//What console output would result from calling main?
+ 
+// answers
+//mystery(a, b, c);	
+//-3 + 1 = 2
+// mystery(c, 4, a);	
+//1 + -3 = 4
+// mystery(a + b, b + c, c + a);	
+//-2 + 3 = -1
